74d5543c7162d998db261099ea8099eb9e00c023
MurrayBen808 <murrayben808@gmail.com>
2023-02-04 00:54:54 -0800

    sound variation script
    random pitch and volume and clip from array

---
+++ Assets/Code/RandomSoundVariant.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSoundVariant : MonoBehaviour
+{
+    //damage sound
+    [Space]
+    [Header("Random Sound")]
+    private AudioSource soundSource;
+    public AudioClip[] soundClip;
+    private AudioClip currentClip;
+    private float volume;
+    private float pitch;
+    public float volumeMin;
+    public float volumeMax;
+    public float pitchMin;
+    public float pitchMax;
+
+    private int index;
+
+
+    public void RandomSoundVariantPlay()
+    {
+        //get random clip
+        index = UnityEngine.Random.Range(0, soundClip.Length);
+        currentClip = soundClip[index];
+
+        //make random volume and pitch
+        volume = UnityEngine.Random.Range(volumeMin, volumeMax);
+        pitch = UnityEngine.Random.Range(pitchMin, pitchMax);
+
+        //set randoms
+        soundSource.volume = volume;
+        soundSource.pitch = pitch;
+
+        //play clip
+        soundSource.PlayOneShot(currentClip);
+    }
+}
