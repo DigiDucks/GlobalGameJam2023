59e93faf03f51c431ba82064947bc808e7a1410e
Jack-Waldron <jack.waldron@digipen.edu>
2023-02-04 03:19:26 -0600

    Preliminary Sprite Work and Follow Camera
    - Replaced player capsule model with protagonist sprite (and also added other sprites to project)
    - Modified camera to be more zoomed in and follow the player closely (stops following perfectly when reaching boundaries of level)

---
+++ Assets/Code/FollowPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject playerRef;
+    private Vector3 diff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        diff = gameObject.transform.position - playerRef.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pt = playerRef.transform.position;
+
+        if(pt.x < -19)
+            pt.x = -19;
+        else if(pt.x > 19)
+            pt.x = 19;
+
+        if(pt.z < -13)
+            pt.z = -13;
+        else if(pt.z > 13)
+            pt.z = 13;
+
+        gameObject.transform.position = pt + diff;
+    }
+}
