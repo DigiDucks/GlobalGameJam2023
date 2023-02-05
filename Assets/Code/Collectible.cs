b6a9b1693db1ffbe647689a8df62b52a568d8a59
Jack-Waldron <jack.waldron@digipen.edu>
2023-02-05 01:29:14 -0600

    Collectibles with New Effects
    - Added Burn, Slow, Chain, and Bomb-related functionality to the Collectible

--- Assets/Code/Collectible.cs
+++ Assets/Code/Collectible.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    [Header("Basic Stats")]
     public int MaxHealthAdded;
     public int ArmorAdded;
     public float SpeedAdded;
@@ -12,6 +13,24 @@ public class Collectible : MonoBehaviour
     public int BulletDamageAdded;
     public float BulletLifetimeAdded;
     public float BulletVelocityAdded;
+
+    [Header("Burn Effect")]
+    public int BurnDamagePerTick;
+    public float BurnDuration;
+    public float BurnTickDuration;
+
+    [Header("Slow Effect")]
+    public float SlowSpeed;
+    public float SlowDuration;
+
+    [Header("Chain Effect")]
+    public int NumChains;
+    public int DamagePerChain;
+    public float ChainRange;
+
+    [Header("Bomb Effect")]
+    public float BombRange;
+    public int BombDamage;
 
     void OnTriggerEnter(Collider coll)
     {
@@ -28,6 +47,20 @@ public class Collectible : MonoBehaviour
             pa.damagePerBullet += BulletDamageAdded;
             pa.bulletLifetime += BulletLifetimeAdded;
             pa.bulletVelocity += BulletVelocityAdded;
+
+            pa.burnDamagePerTick += BurnDamagePerTick;
+            pa.burnDuration += BurnDuration;
+            pa.burnTickDuration += BurnTickDuration;
+
+            pa.slowSpeed += SlowSpeed;
+            pa.slowDuration += SlowDuration;
+
+            pa.numChains += NumChains;
+            pa.damagePerChain += DamagePerChain;
+            pa.chainRange += ChainRange;
+
+            pa.bombRange += BombRange;
+            pa.bombDamage += BombDamage;
 
             Destroy(gameObject);
         }
